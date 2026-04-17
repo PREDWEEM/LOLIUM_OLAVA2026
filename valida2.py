4d9982e8f56d8d9882e8940a44c79be246ed2adc
GCHANTRE <78544473+PREDWEEM@users.noreply.github.com>
2026-04-17 10:11:13 -0300

    Update valida2.py

--- valida2.py
+++ valida2.py
@@ -309,7 +309,7 @@ dga_critico = st.sidebar.number_input("Límite Ventana (°Cd)", value=800, step=
 
 st.sidebar.divider()
 st.sidebar.markdown("## 💧 3. Balance Hídrico (Suelo)")
-w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=30.0, step=1.0)
+w_max_val = st.sidebar.number_input("Cap. de Campo Superficial (mm)", value=20.0, step=1.0)
 
 df_meteo_raw = load_data(archivo_meteo, "meteo_daily")
 df_campo_raw = load_data(archivo_campo, "olava_campo")
